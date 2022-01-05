6e94b5b1534b44c214321c60444b9fbac48d7785
Greenvahn <saulverdemartinez@gmail.com>
2022-01-05 14:03:34 +0000

    implemented basic form validation

--- todo-list/src/components/ToDoForm.js
+++ todo-list/src/components/ToDoForm.js
@@ -2,23 +2,26 @@ import React, {useState} from "react";
 
 const ToDoForm = ({addTask}) => {
     const [ userInput, setUserInput ] = useState('');
+    const [ isValid, setIsValid ] = useState(true)
 
     const handleChange = (e) => {
         setUserInput(e.currentTarget.value)
+        setIsValid(true)
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
         // Do not add an item if the imput is empty
         if(!(userInput.length > 0)) {
-            return false
+            setIsValid(false)
+            return
         }
         addTask(userInput)
         setUserInput('')
     }
 
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className={`form-class ${!isValid ? 'invalid':''}`}>
             <input value={userInput} type="text" onChange={handleChange} placeholder="Enter task..."/>
             <button>Submit</button>
         </form>
