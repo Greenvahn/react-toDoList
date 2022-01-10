58d640c86e39cb737fff8316eabefe45a3a0c1c6
Greenvahn <saulverdemartinez@gmail.com>
2022-01-10 21:55:03 +0000

    unit testing - app.js - more files

--- todo-list/src/__tests__/App.test.js
+++ todo-list/src/__tests__/App.test.js
@@ -1,8 +1,22 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import data from '../rawData/data.json';
 import App from "../App"
 
 describe('<App.js />', () => {
+  it('the task data is correct', () => {
+    expect(data).toMatchSnapshot();
+    expect(data).toHaveLength(data.length);
+    expect(data.map(item => item.task)).toEqual([
+      'Give dog a bath',
+      'Do laundry',
+      'Vacuum floor',
+      'Feed cat',
+      'Change light bulbs'
+    ]);
+  });
+
+  
   it('Renders correctly', () => {
 
     // Arrange
@@ -21,5 +35,35 @@ describe('<App.js />', () => {
     expect(submit_button).toBeInTheDocument();
     expect(form).toBeInTheDocument();
     expect(formInput).toBeInTheDocument();
+  })
+
+  it('Checks on handleToggle function', () =>{
+    // Setup
+    const toDoList = data;
+    let mapped = [];
+    const setToDoList = jest.fn()
+    const handleToggle = jest.fn((value) =>{
+      mapped = toDoList.map(task => {
+        let temp = task.id === Number(value) ? {...task, complete: !task.complete} : {...task}
+        return temp
+      })
+      setToDoList(mapped)
+    })
+
+    render(<App />)
+
+    // Assert
+    const item = screen.getByText("Do laundry")
+
+    // Action
+    fireEvent.click(item)
+    handleToggle(item.id)
+
+
+    // Expectations
+    expect(handleToggle).toHaveBeenCalledWith("2");
+    expect(setToDoList).toHaveBeenCalledWith(mapped);
+    expect(mapped[1].task).toEqual('Do laundry')
+    expect(mapped[1].complete).toEqual(false)
   })
 })
